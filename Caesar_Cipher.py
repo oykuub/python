fcb8ed06d4e631a534ab83ba00d8c67180fea711
Öykü Berfin Öktem <oktemberfin@gmail.com>
2026-02-05 22:22:11 +0300

    Add files via upload

---
+++ Caesar_Cipher.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text,shift_amount,direction):
+    output_text=""
+    if direction=="decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shift_position = alphabet.index(letter) + shift_amount
+            shift_position = shift_position % len(alphabet)
+            output_text += alphabet[shift_position]
+    print(f"Here is the {direction}d result: {output_text}")
+
+should_continue=True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text,shift,direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
